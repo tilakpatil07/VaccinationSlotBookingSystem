0b019eab007b33ed18f743100f6fed01eeb59c73
tilakpatil07 <patiltilak0712@gmail.com>
2024-03-03 21:56:10 +0530

    Methods added to calculate data generation and search time

--- VaccinationSlotBookingSystem/Program.cs
+++ VaccinationSlotBookingSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Net.NetworkInformation;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace VaccinationSlotBookingSystem
 {
@@ -7,120 +8,105 @@ namespace VaccinationSlotBookingSystem
     {
         public static void Main(string[] args)
         {
-            List<int> numberOfHospitals = new List<int> { 100, 1000, 10000, 100000, 1000000, 10000000 };
-            //int numberOfHospitals = 10000;
+            int numberOfHospitals = 100;
             int numberOfDays = 100;
             TestDataGenerator.pinRangeStart = 411001;
             TestDataGenerator.pinRangeEnd = 411005;
             string searchPin;
             Stopwatch stopwatch = new Stopwatch();
+            TimeCalculator timeCalculator = new TimeCalculator();
 
+            //Commented code to calculate data generation time for multiple number of hospitals.
+            //timeCalculator.CalculateDataGenerationTime();
 
-            // Populates hospital list with random data.
-            foreach (int number in numberOfHospitals)
+            //Populates hospital list with random data.
+            List<Hospital> hospitalList = TestDataGenerator.GenerateHospitalList(numberOfHospitals, numberOfDays);
+
+            //Commented code to calculate the search time for given pincode
+            //timeCalculator.CalculateSearchTime(hospitalList);
+
+            Console.WriteLine("Vaccination Center and Slot Availability");
+            Console.WriteLine();
+            while (true)
             {
-                stopwatch.Start();
-                List<Hospital> hospitalList = TestDataGenerator.GenerateHospitalList(number, numberOfDays);
-                stopwatch.Stop();
+                Console.WriteLine("Enter a pincode to search for hospital(s): ");
 
-                Console.WriteLine(number.ToString()+" => "+stopwatch.Elapsed.TotalMilliseconds.ToString());
+                searchPin = CheckPinValidity();
+
+                // Search functionality implementation
+                List<Hospital> hospitalsAtPin = BookingSystem.SearchHospitalsByPin(int.Parse(searchPin), hospitalList);
+
+                if (hospitalsAtPin.Count == 0)
+                {
+                    Console.WriteLine("0 Hospitals found in your area.");
+                    continue;
+                }
+
+                // Displays available hospital and slot details 
+                PrintHospitalDetails(hospitalsAtPin);
+
             }
-            Console.WriteLine("Data saved in file");
+        }
 
+        public static string CheckPinValidity()
+        {
+            string searchPin;
+            while (true)
+            {
+                searchPin = (Console.ReadLine());
 
-            //    Console.WriteLine("Vaccination Center and Slot Availability");
-            //    Console.WriteLine();
-            //    while(true)
-            //    {
+                //Validating if user input is correct
+                if (DataValidator.IsValidPin(searchPin)) break;
+                else
+                {
+                    if (searchPin.Length != 6)
+                    {
+                        Console.WriteLine("The pin you have entered is invalid.\nPlease enter a 6-digit valid pin.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The pin you have entered is invalid.\nPlease enter pin within valid range({TestDataGenerator.pinRangeStart}-{TestDataGenerator.pinRangeEnd}).");
+                    }
+                }
+            }
+            return searchPin;
+        }
 
-            //        Console.WriteLine("Enter a pincode to search for hospital(s): ");
+        public static void PrintHospitalDetails(List<Hospital> hospitalsAtGivenPin)
+        {
+            // Displays Name, address, pin and available slots;
+            Console.WriteLine();
+            Console.WriteLine($"{hospitalsAtGivenPin.Count} hospital(s) found at your provided pincode.");
+            Console.WriteLine();
 
-            //        searchPin = CheckPinValidity();
+            Console.WriteLine("Enter the number of days to display the slots for");
+            int daysToDisplay = int.Parse(Console.ReadLine());
 
-            //        // Starting the time
+            foreach (var hospital in hospitalsAtGivenPin)
+            {
+                Console.WriteLine(hospital.hospitalName);
+                Console.WriteLine(hospital.hospitalAddress);
+                Console.WriteLine(hospital.hospitalPin);
+                Console.WriteLine();
+                PrintSlotDetails(hospital, daysToDisplay);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
 
-            //        stopwatch.Start();
+        public static void PrintSlotDetails(Hospital Hospital, int daysToDisplay)
+        {
+            // Displays date and available number of slots respectively 
+            List<Slot> slotList = Hospital.availableSlotsList;
 
-            //        // Search functionality implementation
-            //        List<Hospital> hospitalsAtPin = BookingSystem.SearchHospitalsByPin(int.Parse(searchPin), hospitalList);
-
-            //        //Stopping the time
-            //        stopwatch.Stop();
-
-
-            //        if (hospitalsAtPin.Count == 0)
-            //        {
-            //            Console.WriteLine("0 Hospitals found in your area.");
-            //            continue;
-            //        }
-
-            //        // Displays available hospital and slot details 
-            //        //PrintHospitalDetails(hospitalsAtPin);
-            //        Console.WriteLine($"{hospitalsAtPin.Count} hospital(s) found at your provided pincode.");
-            //        Console.WriteLine($"Took {stopwatch.Elapsed.TotalMilliseconds.ToString()} milliseconds to search in database of {numberOfHospitals} records. Found {hospitalsAtPin.Count} matching hospital(s).");
-            //    }
-            //}
-
-            //private static string CheckPinValidity()
-            //{
-            //    string searchPin;
-            //    while (true)
-            //    {
-            //        searchPin = (Console.ReadLine());
-
-            //        //Validating if user input is correct
-            //        if (DataValidator.IsValidPin(searchPin)) break;
-            //        else
-            //        {
-            //            if (searchPin.Length != 6)
-            //            {
-            //                Console.WriteLine("The pin you have entered is invalid.\nPlease enter a 6-digit valid pin.");
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine($"The pin you have entered is invalid.\nPlease enter pin within valid range({TestDataGenerator.pinRangeStart}-{TestDataGenerator.pinRangeEnd}).");
-            //            }
-            //        }
-            //    }
-            //    return searchPin;
-            //}
-
-            //public static void PrintHospitalDetails(List<Hospital> hospitalsAtGivenPin)
-            //{
-            //    // Displays Name, address, pin and available slots;
-            //    Console.WriteLine();
-            //    Console.WriteLine($"{hospitalsAtGivenPin.Count} hospital(s) found at your provided pincode.");
-            //    Console.WriteLine();
-
-            //    Console.WriteLine("Enter the number of days to display the slots for");
-            //    int daysToDisplay = int.Parse(Console.ReadLine());
-
-            //    foreach (var hospital in hospitalsAtGivenPin)
-            //    {
-            //        Console.WriteLine(hospital.hospitalName);
-            //        Console.WriteLine(hospital.hospitalAddress);
-            //        Console.WriteLine(hospital.hospitalPin);
-            //        Console.WriteLine();
-            //        PrintSlotDetails(hospital, daysToDisplay);
-            //        Console.WriteLine();
-            //        Console.WriteLine();
-            //    }
-            //}
-
-            //public static void PrintSlotDetails(Hospital Hospital, int daysToDisplay)
-            //{
-            //    // Displays date and available number of slots respectively 
-            //    List<Slot> slotList = Hospital.availableSlotsList;
-
-            //    for (int day = 0; day < daysToDisplay; day++)
-            //    {
-            //        Console.WriteLine($"Date: {slotList[day].date}");
-            //        Console.WriteLine($"Free slots: {slotList[day].numberOfAvailableSlots}");
-            //        Console.WriteLine("----------------------");
-            //    }
-            //    Console.WriteLine();
-            //    Console.WriteLine();
-            //}
+            for (int day = 0; day < daysToDisplay; day++)
+            {
+                Console.WriteLine($"Date: {slotList[day].date}");
+                Console.WriteLine($"Free slots: {slotList[day].numberOfAvailableSlots}");
+                Console.WriteLine("----------------------");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
---
+++ VaccinationSlotBookingSystem/TimeCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationSlotBookingSystem
+{
+    public class TimeCalculator
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        public static List<int> numberOfHospitals = new List<int> {100, 1000, 10000, 100000, 1000000, 10000000};
+        public static int numberOfDays = 100;
+        public void CalculateDataGenerationTime()
+        {
+            foreach (int number in numberOfHospitals)
+            {
+                stopwatch.Start();
+                List<Hospital> hospitalList = TestDataGenerator.GenerateHospitalList(number, numberOfDays);
+                stopwatch.Stop();
+
+                Console.WriteLine(number.ToString() + " => " + stopwatch.Elapsed.TotalMilliseconds.ToString());
+            }
+            Console.WriteLine("Data saved in file");
+        }
+
+        public void CalculateSearchTime(List<Hospital> hospitalList)
+        {
+            Console.WriteLine("Enter the Pin that you want to calculate the search time for: ");
+            string searchPin = Console.ReadLine();
+            // Starting the time
+            stopwatch.Start();
+
+            List<Hospital> hospitalsAtPin = BookingSystem.SearchHospitalsByPin(int.Parse(searchPin), hospitalList);
+
+            //Stopping the time
+            stopwatch.Stop();
+
+            Console.WriteLine($"{hospitalsAtPin.Count} hospital(s) found at your provided pincode.");
+            Console.WriteLine($"Took {stopwatch.Elapsed.TotalMilliseconds.ToString()} milliseconds to search in database of {numberOfHospitals} records. Found {hospitalsAtPin.Count} matching hospital(s).");
+        }
+    }
+}
